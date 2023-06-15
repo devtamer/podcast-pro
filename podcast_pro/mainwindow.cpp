f68405e8c2734713a10c37a88989cfcacfb0c2bb
devtamer <whiletam@gmail.com>
2023-06-15 00:55:09 -0700

    ui changes

--- podcast_pro/mainwindow.cpp
+++ podcast_pro/mainwindow.cpp
@@ -1,11 +1,15 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-
+// constructor of mainwindow class (called when application starts)
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+    , ui(new Ui::MainWindow) // creates new instance of MainWindow class
 {
     ui->setupUi(this);
+
+    // center the widgets in vertical layout
+    ui->centralwidget->
+    ui->verticalLayout->setAlignment(ui->volumeSlider, Qt::AlignCenter);
 }
 
 MainWindow::~MainWindow()
